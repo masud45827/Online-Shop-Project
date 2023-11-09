49ebab4cb68cf125eb8f0972ccf8cf88048bd88d
masud45827 <masud.cse2019@gmail.com>
2023-11-09 17:18:50 +0600

    Online Shop

---
+++ client/src/components/Form/CategoryForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CategoryForm = ({handleSubmit,value,setValue}) => {
+  return (
+    <>
+   <form onSubmit={handleSubmit}>
+  <div className="form-group">
+    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter new category" 
+    value={value}
+    onChange={(e)=> setValue(e.target.value)}/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+    </>
+  )
+}
+
+export default CategoryForm
